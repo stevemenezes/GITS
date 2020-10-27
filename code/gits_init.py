672eefd1e3d44eb24758ffcc7f86faada0592bf2
bhavesh242 <bhavesh.agrawal242@gmail.com>
2020-10-26 18:09:25 -0700

    Added Testing and Functionality for gits_init

---
+++ code/gits_init.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import gits_logging
+from subprocess import Popen, PIPE
+
+
+def gits_init_func(args):
+    """
+    Function that creates an empty Git repository or re-initializes an existing one
+    """
+    try:
+        subprocess_command = list()
+        if args.url:
+            subprocess_command.append("git")
+            subprocess_command.append("clone")
+            subprocess_command.append(args.url)
+        else:
+            subprocess_command.append("git")
+            subprocess_command.append("init")
+            if args.bare is not False:
+                subprocess_command.append("--bare")
+        # print(subprocess_command)
+        process = Popen(subprocess_command, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process.communicate()
+        stderr = stderr.decode("utf-8")
+        print(stderr)
+
+    except Exception as e:
+        print("ERROR: gits init command caught an exception")
+        print("ERROR: {}".format(str(e)))
+        return False
+
+    return True
