b4655d0830b6b57f1e5aa4f51d30ec6423fb00f6
nikithareddyt <nikithareddyt.2307@gmail.com>
2020-10-26 20:33:00 -0400

    pushing code for unit tests

---
+++ test/test_changedir.py
@@ -0,0 +1,9 @@
+import os
+
+currpath = os.path.abspath(".")
+
+if "code" not in currpath:
+    if "test" in currpath:
+        os.chdir(os.path.join("..", "code"))
+    else:
+        os.chdir("code")
